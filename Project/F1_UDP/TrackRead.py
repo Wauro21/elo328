4cc4951d3b73f974aada08f6fc2d024ee0e7bd9f
Mauricio <maurio.aravena@sansano.usm.cl>
2020-11-29 19:14:43 -0300

    Lectura UDP terminada, funcionando. Ploteo finalizado, podria agregarse funcionalidad real time

--- Project/F1_UDP/TrackRead.py
+++ Project/F1_UDP/TrackRead.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 
+#Funcion para obtener puntos x, y, z a partir de archivo .track
 def getValuesTrack(name):
     #almacenar la pista
     x = []
@@ -22,15 +23,41 @@ def getValuesTrack(name):
     file.close()
     return retDict
 
+def getRaceValues(name):
+    x = []
+    y = []
+    z = []
+    file = open(name+".csv", "r")
+    retDict = dict()
+    next(file)
+    #Extraer valores desde archivo
+    for i in file:
+        i = i.strip().split(",")
+        print(i)
+        x.append(float(i[0]))
+        y.append(float(i[1]))
+        z.append(float(i[2]))
+    retDict["x"] = x
+    retDict["y"] = y
+    retDict["z"] = z
+    file.close()
+    return retDict
+
+
 #innerBoundaries = getValuesTrack("silverstone_2020_inner_boundaries")
 centerline = getValuesTrack("silverstone_2020_centerline")
 innerLimit = getValuesTrack("silverstone_2020_innerlimit")
 outerLimit = getValuesTrack("silverstone_2020_outerlimit")
 racingLine = getValuesTrack("silverstone_2020_racingline")
-plt.plot(centerline["x"],centerline["z"],'k-', label = "Línea central")
+#plt.plot(centerline["z"],centerline["x"],'k-', label = "Línea central")
 #plt.plot(innerBoundaries["x"],innerBoundaries["z"])
-plt.plot(innerLimit["x"],innerLimit["z"],'b-', label = "Límite interno")
-plt.plot(outerLimit["x"],outerLimit["z"], 'g-', label = 'Límite externo')
-plt.plot(racingLine["x"],racingLine["z"], 'r--', label = "Línea carrera")
+plt.plot(innerLimit["z"],innerLimit["x"],'b-', label = "Límite interno")
+plt.plot(outerLimit["z"],outerLimit["x"], 'g-', label = 'Límite externo')
+#plt.plot(racingLine["z"],racingLine["x"], 'r--', label = "Línea carrera")
+
+#Prueba de concepto
+testRace = getRaceValues("testing")
+plt.plot(testRace["x"],testRace["z"],'m--', label = "Testing")
+
 plt.legend()
 plt.show()
--- Project/F1_UDP/udpReader.py
+++ Project/F1_UDP/udpReader.py
@@ -1,4 +1,30 @@
 import socket
+import ctypes
+import sys
+#Variables globales
+
+headerSize = 21 # en bytes
+mPacketSize = 60 # en bytes
+#Clase para el desempaquetado del Header usando ctypes, littleEndian
+class packetHeader(ctypes.LittleEndianStructure):
+    _pack_ = 1
+    _fields_ = [    ("m_packetFormat",ctypes.c_uint,16),
+                    ("m_packetVersion",ctypes.c_uint8,8),
+                    ("m_packetId",ctypes.c_uint8,8),
+                    ("m_sessionUID",ctypes.c_uint64,64),
+                    ("m_sessionTime",ctypes.c_float),
+                    ("m_frameIdentifier",ctypes.c_uint32,32),
+                    ("m_playerCarIndex",ctypes.c_uint8,8),
+    ]
+
+#Clase para el desempaquetado del motionPacket usando ctypes, littleEndian
+class motionPacket(ctypes.LittleEndianStructure):
+    _pack_ = 1
+    _fields_ = [    ("m_worldPositionX", ctypes.c_float),
+                    ("m_worldPositionY", ctypes.c_float),
+                    ("m_worldPositionZ", ctypes.c_float),
+
+    ]
 
 print("UDP - Reading")
 
@@ -11,14 +37,15 @@ sock.bind((UDP_IP, UDP_PORT))
 
 
 # 1) Seleccionar packet UDP de interes:
-# 2) Desempaquetar información importante
-# 3) Guardar la información en un archivo .csv (Verificar)
+# 2) Desempaquetar informacion importante
+# 3) Guardar la informacion en un archivo .csv (Verificar)
 # 4) Forma de terminar la lectura UDP de forma sana
 
 # Llega el LSB primero, de todo numero
 
 while True:
     data, addr = sock.recvfrom(2048)
-    if(len(data) == 1341): #Tamaño motion packet
-        #print("Recibido: {!r}",format(data))
-        print(data[0:2])
+    if(len(data) == 1341): #Tamano motion packet
+        damn = packetHeader.from_buffer_copy(data[0:headerSize+1])
+        damn_2 = motionPacket.from_buffer_copy(data[headerSize:mPacketSize+1])
+        print("{},{},{}".format(damn_2.m_worldPositionX,damn_2.m_worldPositionY,damn_2.m_worldPositionZ))
