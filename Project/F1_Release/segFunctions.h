74efff9bd5b182fce7ef33ab45d3a6664dcbd8e8
Diego <diego.badillo@sansano.usm.cl>
2021-01-06 14:59:52 -0300

    comentarios segFunctions.h en formato doxygen

--- Project/F1_Release/segFunctions.h
+++ Project/F1_Release/segFunctions.h
@@ -17,17 +17,52 @@
 
 #define ATTEMPTS 1
 
+/*---------------------------------------------------------------------------*/
+/*
+ * @brief       : oculta líneas del trapecio generado al aplicar Canny a la proyección
+ * @param src   : matriz a editar
+ * @param width : ancho de las líneas
+ */
 void drawLine(cv::Mat& src, int width);
-// elimina lineas del trapecio generado por la proyeccion
+/*---------------------------------------------------------------------------*/
+/*
+ * @brief       : apertura morfológica
+ * @param src   : matriz fuente
+ * @param dst   : matriz destino
+ * @param x     : ancho elemento estructurante (elípse)
+ * @param y     : altura elemento estructurante (elípse)
+ */
 void opening(cv::Mat& src, cv::Mat& dst, int x, int y);
-// apertura morfologica
+/*---------------------------------------------------------------------------*/
+/*
+ * @brief       : cierre morfológico
+ * @param src   : matriz fuente
+ * @param dst   : matriz destino
+ * @param x     : ancho elemento estructurante (elípse)
+ * @param y     : altura elemento estructurante (elípse)
+ */
 void closing(cv::Mat& src, cv::Mat& dst, int x, int y);
-// cierre morfologico
+/*---------------------------------------------------------------------------*/
+/*
+ * @brief       : oculta las ruedas en la proyección
+ * @param src   : matriz fuente
+ */
 void hideWheels(cv::Mat& src);
-// oculta ruedas
+/*---------------------------------------------------------------------------*/
+/*
+ * @brief       : superpone la máscara a la imagen de entrada (1080p)
+ * @param src   : matriz fuente (imagen de entrada)
+ * @param alpha : ponderación de src
+ * @param ROI   : máscara
+ * @param beta  : ponderación de ROI
+ */
 void addMask(cv::Mat src, double alpha, cv::Mat& ROI, double beta);
-// solo funciona para 1080p (por mientras)
+/*---------------------------------------------------------------------------*/
+/*
+ * @brief       : segmentación de bordes mediante Canny y operaciones morfológicas
+ * @param crop  : matriz fuente
+ * @return      : matriz correspondiente a la segmentación de crop
+ */
 cv::Mat getEdges(cv::Mat crop);
-// obtiene bordes de la imagen
-
+/*---------------------------------------------------------------------------*/
 #endif /* SEGFUNCTIONS_H */
